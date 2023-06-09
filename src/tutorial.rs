40d4e67406c70e1cbd0396d59b99d52129138860
Sboonny <muhammedelruby@gmail.com>
2023-06-10 00:07:33 +0300

    parse CLI arguments

--- src/tutorial.rs
+++ src/tutorial.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Cli::parse();
+    let args: Vec<String> = std::env::args().collect();
+
+    println!("My path is {}.", args[0]);
+    println!("I got {:?} arguments: {:?}.", args.len() - 1, &args[1..]);
 }
