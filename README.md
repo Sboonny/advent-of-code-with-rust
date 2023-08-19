b5499a3f572e30d5f7fb1dbf5872c62fde8b3d14
Sboonny <muhammedelruby@gmail.com>
2023-08-20 00:34:08 +0300

    add anyhow and use it

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 cargo run
 ```
 
+The tutorial is from [CLI in Rust](https://rust-cli.github.io/book/tutorial/index.html) docs. To run it, run:
+
+```console
+cargo run -- main src/tutorial.rs
+```
+
 ---
 
 - To run different files, change the `path` value in `Cargo.toml`. 
--- src/tutorial.rs
+++ src/tutorial.rs
@@ -1,4 +1,5 @@
 use std::io::{BufReader, BufRead};
+use anyhow::{Context, Result};
 use std::fs::File;
 use clap::Parser;
 #[derive(Parser)]
@@ -9,7 +10,7 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let file = File::open(&args.path).expect("could not read file");
+    let file = File::open(&args.path).with_context(|| format!("could not read file `{}`", args.path.display()))?;
     let content = BufReader::new(file);
     for line in content.lines() {
         match line {
