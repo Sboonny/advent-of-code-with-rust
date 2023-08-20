b5ec9c9cc01ba7b8db128287a61fbe7a1466f2f9
Sboonny <muhammedelruby@gmail.com>
2023-08-21 02:23:58 +0300

    add logging and how to run it

--- README.md
+++ README.md
@@ -4,10 +4,22 @@
 cargo run
 ```
 
-The tutorial is from [CLI in Rust](https://rust-cli.github.io/book/tutorial/index.html) docs. To run it, run:
+- The tutorial is from [CLI in Rust](https://rust-cli.github.io/book/tutorial/index.html) docs. To run it, run:
 
 ```console
 cargo run -- main src/tutorial.rs
+```
+
+- To log, run:
+
+```
+<!-- windows -->
+set RUST_LOG=info
+
+<!-- Linux and MacOs -->
+env RUST_LOG=info
+
+cargo run --bin output-log
 ```
 
 ---
--- src/tutorial.rs
+++ src/tutorial.rs
@@ -2,6 +2,7 @@ use std::io::{BufReader, BufRead};
 use anyhow::{Context, Result};
 use std::fs::File;
 use clap::Parser;
+use log::{info, warn};
 #[derive(Parser)]
 struct Cli {
     pattern: String,
@@ -9,16 +10,20 @@ struct Cli {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
     let args = Cli::parse();
     let file = File::open(&args.path).with_context(|| format!("could not read file `{}`", args.path.display()))?;
+    info!("Reading the file.");
     let content = BufReader::new(file);
     for line in content.lines() {
         match line {
             Ok(line) => {
                 println!("{}", line);
             },
-            Err(error) => { return Err(error.into()); }
+            Err(error) => { return {
+                Err(error.into())}; }
         }
     }
+    warn!("oops, issue happened while reading the file.");
     Ok(())
 }
