5067614aabcd706a340b453fe677ee2ea0743cd1
Sboonny <muhammedelruby@gmail.com>
2023-10-27 17:45:50 +0200

    finish the first part
    Finding the md-5 package was a nightmare

--- src/advent-of-code15/day-four.rs
+++ src/advent-of-code15/day-four.rs
@@ -1,4 +1,3 @@
-// output is bgvyzdsv
 // https://adventofcode.com/2015/day/4
 
 // Santa needs help mining some AdventCoins (very similar to bitcoins) to use as gifts for all the economically forward-thinking little girls and boys.
@@ -9,8 +8,24 @@
 
 // If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
 // If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
+use md5::{Md5, Digest};
 
 fn main() {
-    
+    let decoded_string: &str = "bgvyzdsv";
+    let pattern =  "00000";
+    let mut i = 0;
+    let mut found = false;
+    while !found {
+        let mut hasher = Md5::new();
+        hasher.update(decoded_string.as_bytes());
+        hasher.update(i.to_string().as_bytes());
+        let result = hasher.finalize();
+        let result_string = format!("{:x}", result);
+        if result_string.starts_with(pattern) {
+            found = true;
+            println!("Found it! {}", i);
+        }
+        i += 1;
+    }
 }
 
