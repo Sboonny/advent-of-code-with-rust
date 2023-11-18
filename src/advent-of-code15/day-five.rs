53b028cd5df749a0705fee01a37e06ef6a3b10b9
Sboonny <muhammedelruby@gmail.com>
2023-11-18 19:28:57 +0200

    day five part 2

--- src/advent-of-code15/day-five.rs
+++ src/advent-of-code15/day-five.rs
@@ -1,48 +1,51 @@
-// https://adventofcode.com/2015/day/5
-// Santa needs help figuring out which strings in his text file are naughty or nice.
+// Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
 
-// A nice string is one with all of the following properties:
+// Now, a nice string is one with all of the following properties:
 
-// It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
-// It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
-// It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+// It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+// It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
 // For example:
 
-// ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), and none of the disallowed substrings.
-// aaa is nice because it has at least three vowels and a double letter, even though the letters used by different rules overlap.
-// jchzalrnumimnmhp is naughty because it has no double letter.
-// haegwjzuvuyypxyu is naughty because it contains the string xy.
-// dvszwmarrgswjxmb is naughty because it contains only one vowel.
-// How many strings are nice?
+// qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+// xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+// uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+// ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+// How many strings are nice under these new rules?
 
 fn main() {
     let input: String = std::fs::read_to_string("src/advent-of-code15/day-five-input.txt")
         .expect("couldn't read the file, check if day-three-input.txt exists.");
 
-    let vowels: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
-    let forbidden_char: [&str; 4] = ["ab", "cd", "pq", "xy"];
-    let mut the_amount_of_nice_strings: i32 = 0;
+    let mut nice_strings = 0;
 
     for line in input.lines() {
-        let mut vowel_count: i32 = 0;
-        let mut double_letter: bool = false;
-        let mut forbidden_string: bool = false;
-        let mut last_char: char = ' ';
-        for c in line.chars() {
-            if vowels.contains(&c) {
-               vowel_count += 1;
+        let chars: Vec<char> = line.chars().collect();
+        let mut pair_found = false;
+        let mut repeat_found = false;
+
+        for i in 0..chars.len() - 1 {
+            let pair = &chars[i..i + 2];
+
+            if i < chars.len() - 2 && chars[i] == chars[i + 2] {
+                repeat_found = true;
             }
-            if c == last_char {
-                double_letter = true;
+
+            for j in i + 2..chars.len() - 1 {
+                if &chars[j..j + 2] == pair {
+                    pair_found = true;
+                    break;
+                }
             }
-            if forbidden_char.contains(&format!("{}{}", last_char, c).as_str()) {
-                forbidden_string = true;
+
+            if pair_found && repeat_found {
+                break;
             }
-            last_char = c;
         }
-        if vowel_count >= 3 && double_letter && !forbidden_string {
-            the_amount_of_nice_strings += 1;
+
+        if pair_found && repeat_found {
+            nice_strings += 1;
         }
     }
-    print!("The amount of nice strings is: {}", the_amount_of_nice_strings)
+
+    println!("Number of nice strings: {}", nice_strings);
 }
